982246967977b941f05c9e70465f570311351113
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2016-06-02 19:35:25 +0200

    BPT-150: Renamed to "RandomDuration".

--- src/main/java/info/novatec/tr/delegate/RandomDuration.java
+++ src/main/java/info/novatec/tr/delegate/RandomDuration.java
@@ -5,11 +5,14 @@ import javax.inject.Named;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+/**
+ * Random duration between 0 and 10 seconds.
+ */
 @Named
-public class Level1ServiceTask implements JavaDelegate {
+public class RandomDuration implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		Thread.sleep(new Double(Math.random()*60000).longValue());
+		Thread.sleep((long)Math.random()*10000);
 	}
 }
