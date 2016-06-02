995bb6aa1f3ac60dd537d552cd5b4eb27f53e9ad
Steve Walter <steve.walter@novatec-gmbh.de>
2016-06-02 18:39:27 +0200

    BPT-148: Configured Initial User Usage

--- src/main/java/info/novatec/tr/CreateDefaultUser.java
+++ src/main/java/info/novatec/tr/CreateDefaultUser.java
@@ -2,8 +2,11 @@ package info.novatec.tr;
 
 import javax.annotation.PostConstruct;
 
+import org.camunda.bpm.admin.impl.web.SetupResource;
 import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.identity.User;
+import org.camunda.bpm.engine.rest.dto.identity.UserCredentialsDto;
+import org.camunda.bpm.engine.rest.dto.identity.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,15 +25,21 @@ public class CreateDefaultUser {
 
 
 	@PostConstruct
-	public void createUser() {
+	public void ensureInitialUserIsCreated() {
+
 		if (identityService.createUserQuery().userId("demo").count() > 0)
 			return;
 
 		User demo = identityService.newUser("demo");
 		demo.setFirstName("John");
 		demo.setLastName("Rambo");
-		demo.setPassword("demo");
 
-		identityService.saveUser(demo);
+		UserDto fromUser = UserDto.fromUser(demo, true);
+
+		UserCredentialsDto credentials = new UserCredentialsDto();
+		credentials.setPassword("demo");
+		fromUser.setCredentials(credentials);
+
+		new SetupResource().createInitialUser("default", fromUser);
 	}
 }
