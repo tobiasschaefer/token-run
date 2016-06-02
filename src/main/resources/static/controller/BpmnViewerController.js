74c7bbf9cd5c9d8d8ea113569de498fea74b8c79
kerstinguenther <Kerstin.Guenther@student.hpi.uni-potsdam.de>
2016-06-02 15:48:37 +0200

    BPT-144: Steuerelemente hinzugefuegt

--- src/main/resources/static/controller/BpmnViewerController.js
+++ src/main/resources/static/controller/BpmnViewerController.js
@@ -1,12 +1,19 @@
 (function(){
 	'use strict';
 
-	angular.module('TokenRunApp').controller('BpmnViewerController', ['$scope', function($scope) {
+	angular.module('TokenRunApp').controller('BpmnViewerController', ['$scope', '$window', function($scope, $window) {
 
-		var BpmnViewer = window.BpmnJS;
-		var viewer = new BpmnViewer({ container: angular.element('#js-canvas'), height: 600 });
+		var height = $window.innerHeight - 200;
+		
+		var BpmnNavigatedViewer  = window.BpmnJS;
+		var viewer = new BpmnNavigatedViewer ({ container: angular.element('#js-canvas'), height });
 
 		var url = "../ressources/test-process.xml";
+		
+		$scope.level = {
+				name: "Test-Prozess",
+				zeit: "00:00:00"
+		};
 		
 		$.ajax(url, { dataType : 'text' }).done(function(xml) {
 			viewer.importXML(xml, function(err) {
@@ -17,5 +24,16 @@
 				}
 			});
 		});
+		
+		$scope.startLevel = function() {
+			// TODO: xml aus REST-API holen
+			// TODO: Prozess starten
+			// TODO: Zeit starten
+			$scope.level.zeit = "00:00:01";
+		}
+		
+		$scope.abbruch = function() {
+			// TODO: zurück auf Startseite
+		}
 	}]);
 }());
