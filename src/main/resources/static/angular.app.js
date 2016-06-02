30e35af1180637030315d18ce056aa5569758a1a
kerstinguenther <Kerstin.Guenther@student.hpi.uni-potsdam.de>
2016-06-02 14:21:20 +0200

    BPT-138: test process model wird angezeigt

---
+++ src/main/resources/static/angular.app.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+
+  angular.module('TokenRun', []);
+
+// hier können App-weite Einstellungen vorgenommen werden, indem Konstanten mit .const gesetzt werden
+
+}());
---
+++ src/main/resources/static/controller/BpmnViewerController.js
@@ -0,0 +1,21 @@
+(function(){
+	'use strict';
+
+	angular.module('TokenRun').controller('BpmnViewerController', ['$scope', function($scope) {
+
+		var BpmnViewer = window.BpmnJS;
+		var viewer = new BpmnViewer({ container: angular.element('#js-canvas'), height: 600 });
+
+		var url = "../ressources/test-process.xml";
+		
+		$.ajax(url, { dataType : 'text' }).done(function(xml) {
+			viewer.importXML(xml, function(err) {
+				if (err) {
+					console.error(err);
+				} else {
+					viewer.get('canvas').zoom('fit-viewport');
+				}
+			});
+		});
+	}]);
+}());
