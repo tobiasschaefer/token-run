7d8729b08ebfb1cf1c9ee73217689ca30f1bf095
Eric Klieme <ekl@novatec-gmbh.de>
2016-06-02 22:28:49 +0200

    BPT-159: added highscore view for start page based on level selection

--- src/main/resources/static/controller/GameActionsController.js
+++ src/main/resources/static/controller/GameActionsController.js
@@ -1,15 +1,18 @@
 (function(){
 	'use strict';
 
-	angular.module('TokenRunApp').controller('GameActionsController', ['restUrlProcessDefinitions', '$http', '$scope','$window', function (restUrlProcessDefinitions, $http, $scope,$window ) {
+	angular.module('TokenRunApp').controller('GameActionsController', ['restUrlProcessDefinitions', '$http', '$scope','$window','$timeout', function (restUrlProcessDefinitions, $http, $scope,$window,$timeout) {
 
 		var defaultLevelSelection = {
-				name:"Level auswählen",
+				name:"choose level",
 				id:-1
 		};
 
 
 		$scope.selectedProcessDefinition = defaultLevelSelection;
+		$scope.levelHighscores = [];
+		
+		$scope.showLog = false;
 
 		$scope.error = "";
 		$scope.info = "";
@@ -20,6 +23,10 @@
 			$scope.error = "";
 			$scope.info = "";
 		}
+		
+		var resetHighscores = function() {
+			$scope.levelHighscores = [];
+		}
 
 		$scope.loadAvailableProcessDefinitions = function() {
 
@@ -29,7 +36,7 @@
 				method: 'GET',
 				url: restUrlProcessDefinitions
 			}).then(function successCallback(response) {
-				console.log("found "+response.data.length+" deployed process definitions");
+				console.debug("found "+response.data.length+" deployed process definitions");
 				if(response.data.length>0) {
 					for(var i in response.data) {
 						$scope.processDefinitions.push({
@@ -38,15 +45,41 @@
 						});
 					}
 				} else {
-					$scope.info="Keine Level zum Spielen!";
+					$scope.info="no level to play!";
 				}
 			}, function errorCallback(response) {
 				$scope.error = "TokenRun Error occured while accessing "+restUrlProcessDefinitions+" - status: "+response.status;
 			});
+			
+			$timeout(function() {
+				$scope.showLog = true;
+			}, 60000);
 		}
 		
 		$scope.playLevel = function() {
 			$window.location.href="BpmnViewer.html?processDefinitionKey="+$scope.selectedProcessDefinition.name;
+		}
+		
+		$scope.showHighscoresForLevel = function() {
+			
+			resetStateInformation();
+			resetHighscores();
+			
+			$http({
+				method: 'GET',
+				url: '/'+$scope.selectedProcessDefinition.name+'/scores/10'
+			}).then(function successCallback(response) {
+				console.log("found "+response.data.length+" level scores (max 10)");
+				if(response.data.length>0) {
+					$scope.levelHighscores = response.data;
+					$('#highscoreModal').modal('toggle');
+					
+				} else {
+					$scope.info = "No highscores for "+$scope.selectedProcessDefinition.name+" available";
+				}
+			}, function errorCallback(response) {
+				$scope.error = "TokenRun Error occured while accessing "+restUrlProcessDefinitions+" - status: "+response.status;
+			});
 		}
 
 	}]);
