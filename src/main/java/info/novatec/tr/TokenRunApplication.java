0454551cead14092ecd58223d324a28b859f5187
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2016-06-02 10:54:42 +0200

    BPT-123: Add initial spring boot project based on web.

---
+++ src/main/java/info/novatec/tr/TokenRunApplication.java
@@ -0,0 +1,12 @@
+package info.novatec.tr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenRunApplication.class, args);
+	}
+}
